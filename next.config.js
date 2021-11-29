67637111e826c7251cef89394821d2a7328701e2
slimkhemiri <khemiri.medslim1@gmail.com>
2021-11-29 15:28:29 +0000

    nav bar

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
-    domains: ["images.squarespace-cdn.com", "www.kindpng.com"],
+    domains: [
+      "images.squarespace-cdn.com",
+      "mpng.subpng.com",
+      "www.kindpng.com",
+      "png.pngtree.com",
+      "toppng.com",
+    ],
   },
 };
