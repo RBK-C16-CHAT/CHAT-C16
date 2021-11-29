9f022ba4258a1d5acfcfa49cab31cdb6e99f4cd6
slimkhemiri <khemiri.medslim1@gmail.com>
2021-11-29 13:32:35 +0000

    pull the work

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,11 @@
 function Card() {
   const posts = ["post1", "post2", "post3", "post4"];
 
-  
-
   return (
     <div className="w-96 p-80 mx-96 border border-gray-900">
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <div>{post}</div>
-      ))}
+      ))} */}
 
       <div className="flex flex-row-auto">
         <input
